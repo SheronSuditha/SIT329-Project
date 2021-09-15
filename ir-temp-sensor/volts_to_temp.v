11ac6f5419e7b2ba0dfaf8f1caaded6bcaf75ca2
Brenton Fleming <bkfl@deakin.edu.au>
2021-09-15 20:45:33 +1000

    Add voltage to temperature scaling

--- ir-temp-sensor/volts_to_temp.v
+++ ir-temp-sensor/volts_to_temp.v
@@ -1,5 +1,11 @@
 module volts_to_temp (
-	input voltage
+	input [11:0] adc_input,
+	output reg [7:0] temperature
 );
+
+// Scale adc_input range from 0 - 4095 to 0 - 100 temperature range
+always @(*) begin
+	temperature <= adc_input / 41;	
+end
 
 endmodule
